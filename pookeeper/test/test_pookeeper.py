44bb4a7c0647668f14a1eb91485f8f4d0d254b0f
Nhat Bui <nhatbui@Nhats-MacBook-Pro.local>
2017-04-08 20:48:14 -0700

    Add watching unit test. Factory is testcase attribute

--- pookeeper/test/test_pookeeper.py
+++ pookeeper/test/test_pookeeper.py
@@ -6,10 +6,11 @@ from twisted.test import proto_helpers
 class PooKeeperTestCase(unittest.TestCase):
 
     def setUp(self):
-        factory = ZooKeeperFactory()
-        self.proto = factory.buildProtocol(('127.0.0.1', 0))
+        self.factory = ZooKeeperFactory()
+        self.proto = self.factory.buildProtocol(('127.0.0.1', 0))
         self.tr = proto_helpers.StringTransport()
         self.proto.makeConnection(self.tr)
+        self.assertTrue(self.tr.value(), 'true:Connected\r\n')
 
 
     def addNode(self, node):
@@ -70,7 +71,34 @@ class PooKeeperTestCase(unittest.TestCase):
         self.tr.clear()
         self.proto.lineReceived('CHILDREN:/')
         self.assertEqual(self.tr.value(), 'nhat\r\n')
-        
+
+
+    def test_watch(self):
+        self.addNode('/nhat')
+
+        # Make new client who cares about this node
+        new_proto = self.factory.buildProtocol(('127.0.0.1', 1))
+        new_tr = proto_helpers.StringTransport()
+        new_proto.makeConnection(new_tr)
+
+        # New client watches node
+        new_proto.lineReceived('WATCH:/nhat')
+
+        # Delete this node
+        self.deleteNode('/nhat')
+
+        # Check if client got a notice
+        # NOTE: during our checks there's no '\n' char because split()
+        # removed it.
+        client_msgs = new_tr.value().split('\n')
+        self.assertEqual(client_msgs[1], 'true:WATCHING:/nhat\r')
+        self.assertEqual(client_msgs[2], 'true:WATCHER_NOTICE:DELETED:/nhat\r')
+
+        print('new_tr')
+        print(new_tr.value())
+        print('self.tr')
+        print(self.tr.value())
+
 
 if __name__ == '__main__':
     unittest.main()
